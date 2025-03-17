db3876b5ee5e42535f9150a37597e10d39953874
Andrew Demarest <drewdemarest@gmail.com>
2025-03-17 13:40:04 -0500

    DEV: Add a functioning Contact page
    The contact page now loads the post specified in config.json rather than
    a generic email to yonderserver.com

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,19 @@
+import Post from "../components/post/post"
 import Navbar from "../components/navbar/navbar";
+import { promises as fs } from 'fs';
 
-export default function ContactPage(){
+async function loadConfig() {
+  return fs.readFile(process.cwd() + '/config.json', 'utf-8').then(content => JSON.parse(content));
+}
+
+export default async function ContactPage(){
+    let config = await loadConfig();
+    console.log(config);
+    console.log(config.apiServerURI);
     return (
         <div className="ContactPage">
             <Navbar/>
-            <h1>Email: contact@yonderserver.com</h1>
+            <Post apiServerURI={config.apiServerURI} postId={config.contactPostId}/>
         </div>
     )
 }
